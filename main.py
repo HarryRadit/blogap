c0cbc80fb081ebeccc1303dd711bcd1fb87e0105
HarryRadit <harrydxm@gmail.com>
2024-11-09 19:24:31 +0700

    blog

--- main.py
+++ main.py
@@ -9,7 +9,19 @@ def home():
 
 @app.route('/post')
 def post():
-    return render_template('single.html')
+    return render_template('post.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
