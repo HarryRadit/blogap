95ffe3c4a6a01a51be08f2e24a17e62f9871b1f0
HarryRadit <harrydxm@gmail.com>
2024-11-16 19:23:00 +0700

    blog

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from flask import Flask, render_template, request, redirect, url_for
-
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sysql://root@localhost/blogapp'
+db = SQLAlchemy(app)
 
+class  Contact(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False)
+    message = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, nullable=False)
 @app.route('/')
 def home():
     return render_template('index.html')
