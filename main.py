4120ef9a21c437456cbcbe3d0c216317b9e76af2
HarryRadit <harrydxm@gmail.com>
2024-11-18 19:24:48 +0700

    blog

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
+import json
+
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from datetime import *
 app = Flask(__name__)
 
+with open("config.json", "r") as c:
+    params = json.load(c)["parameters"]
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost/blogapp'
+if params['local_server']:
+    app.config['SQLALCHEMY_DATABASE_URI'] = params['local_uri']
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = params['prod_uri']
+
+SECRET_KEY = params['secret_key']
+app.config['SECRET_KEY']
 db = SQLAlchemy(app)
+
 
 
 class Contact(db.Model):
@@ -12,9 +24,12 @@ class Contact(db.Model):
    name = db.Column(db.String(100), nullable=False)
    email = db.Column(db.String(100), nullable=False)
    message = db.Column(db.Text, nullable=False)
+   date = db.Column(db.String(20), nullable = True)
 
 
-
+@app.route('/login')
+def login():
+   return render_template('login.html', param=params)
 @app.route('/')
 def home():
    return render_template('index.html')
@@ -30,13 +45,13 @@ def about():
    return render_template('about.html')
 
 
-@app.route('/contact')
+@app.route('/contact', methods=['GET', 'POST'])
 def contact():
    if request.method == 'POST':
        name = request.form.get('name')
        email = request.form.get('email')
        Msg = request.form.get('message')
-       entry = Contact(name=name, email=email, message=Msg)
+       entry = Contact(name=name, email=email, message=Msg, date = datetime.today().date())
        db.session.add(entry)
        db.session.commit()
    return render_template('contact.html')
@@ -50,7 +65,7 @@ def login():
 if __name__ == '__main__':
    with app.app_context():
        db.create_all()
-       app.run(debug=True, port=3306)
+       app.run(debug=True, port=8080)
 
 
 
